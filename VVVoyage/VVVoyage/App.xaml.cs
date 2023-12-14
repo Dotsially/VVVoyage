92b5d809b153b978b5835577605ee8cd8c3be48b
jewiegel <118447630+jewiegel@users.noreply.github.com>
2023-12-14 10:40:08 +0100

    Add files via upload

---
+++ VVVoyage/VVVoyage/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VVVoyage
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ VVVoyage/VVVoyage/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace VVVoyage
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
