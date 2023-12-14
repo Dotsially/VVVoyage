5507509dd0b38499d9db0f9c773c551343b7c988
jewiegel <j.wiegel@student.avans.nl>
2023-12-14 15:17:00 +0100

    added mvvm skeleton

---
+++ VVVoyage/VVVoyage/Views/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Domain;
+using System.Diagnostics;
+
+namespace VVVoyage
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+
+            Debug.WriteLine(CounterBtn.Text);
+        }
+    }
+
+}
