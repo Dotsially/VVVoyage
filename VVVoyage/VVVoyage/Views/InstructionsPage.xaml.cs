2cfa15c09972a39db8d77b7a0e08ae46c9a2de70
Simon <saj.decock@student.avans.nl>
2023-12-16 15:04:33 +0100

    Implement back button and page routing for instructions page

--- VVVoyage/VVVoyage/Views/InstructionsPage.xaml.cs
+++ VVVoyage/VVVoyage/Views/InstructionsPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class InstructionsPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	public async void BackBtn_Clicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync("MainPage");
+	}
 }
