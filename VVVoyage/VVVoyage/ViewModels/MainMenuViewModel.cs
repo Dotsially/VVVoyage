9f29925a9b33a4e296ea9dabd2c22b9ee6aee966
jewiegel <j.wiegel@student.avans.nl>
2023-12-19 12:14:51 +0100

    added Localization directory with AppResources.resx

--- VVVoyage/VVVoyage/ViewModels/MainMenuViewModel.cs
+++ VVVoyage/VVVoyage/ViewModels/MainMenuViewModel.cs
@@ -14,11 +14,11 @@ namespace VVVoyage.ViewModels
     {
         private static readonly List<Sight> tour1Landmarks =
         [
-            new("Old VVV building", new Location(51.592412225120796, 4.775692730162895), "The building in which the VVV used to be housed inside."),
-            new("Liefdeszusters", new Location(51.59336561016905, 4.779405797254084), "Description of the Liefdeszusters landmark."),
-            new("Nassau Baronie Monument", new Location(51.59268164269348, 4.779718410749389), "Description of the Nassau Baronie landmark."),
-            new("The Light House", new Location(51.584039168945026, 4.774673039583854), "Description of the light house."),
-            new("Kasteel van Breda", new Location(51.59108157152743, 4.776103712549693), "Description of the Castle of Breda."),
+            new("Old VVV building", new Location(51.592412225120796, 4.775692730162895), Resources.Localization.AppResources.Old_VVV_Description),
+            new("Liefdeszusters", new Location(51.59336561016905, 4.779405797254084), Resources.Localization.AppResources.Liefdeszusters_Description),
+            new("Nassau Baronie Monument", new Location(51.59268164269348, 4.779718410749389), Resources.Localization.AppResources.Nassau_Description),
+            new("The Light House", new Location(51.584039168945026, 4.774673039583854), Resources.Localization.AppResources.Light_House_Description),
+            new("Kasteel van Breda", new Location(51.59108157152743, 4.776103712549693), Resources.Localization.AppResources.Castle_Description),
         ];
 
         private static readonly List<Sight> tour2Landmarks =
