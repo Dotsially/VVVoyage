2dae77b3cfaae56830891d00f701729cc078a6d7
Meindert Kempe <mail@mkempe.com>
2023-12-20 15:30:10 +0100

    Fix SQLAppDatabase

--- VVVoyage/VVVoyage/Database/SQLAppDatabase.cs
+++ VVVoyage/VVVoyage/Database/SQLAppDatabase.cs
@@ -9,24 +9,29 @@ namespace VVVoyage.Database;
 public class SQLAppDatabase : IAppDatabase
 {
     private const string FILENAME = "vvvoyage.db";
-    private SQLiteAsyncConnection _database;
+    private readonly SQLiteAsyncConnection _database;
 
-    public async Task Init()
+    public SQLAppDatabase()
     {
-        if (_database != null)
-            return;
-
         const SQLiteOpenFlags flags = SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.SharedCache |
                                       SQLiteOpenFlags.FullMutex;
 
         string path = Path.Combine(FileSystem.AppDataDirectory, FILENAME);
 
         _database = new SQLiteAsyncConnection(path, flags);
+    }
 
+    public async Task Init()
+    {
         try
         {
-            _ = await _database.CreateTableAsync<Landmark>();
-            _ = await _database.CreateTableAsync<Route>();
+            await _database.CreateTableAsync<Landmark>()
+                .ContinueWith((res) => Debug.WriteLine("Created table Landmarks"));
+            await _database.CreateTableAsync<Route>()
+                .ContinueWith((res) => Debug.WriteLine("Created table Route"));
+            //resLandmark.Start();
+            //resRoute.Start();
+            //await Task.WhenAll(resLandmark, resRoute);
         }
         catch (Exception e)
         {
@@ -35,7 +40,7 @@ public class SQLAppDatabase : IAppDatabase
         }
     }
 
-    public async Task close()
+    public async Task Close()
     {
         await _database.CloseAsync();
     }
@@ -65,8 +70,10 @@ public class SQLAppDatabase : IAppDatabase
         r.Name = tour.Name;
         r.Description = tour.Description;
         StringBuilder builder = new StringBuilder();
+        //for (int i = 0; i < tour.Landmarks.Count; ++i)
         foreach (Sight sight in tour.Landmarks)
         {
+            //Sight sight = tour.Landmarks[i];
             List<Landmark> lm;
             try
             {
@@ -75,6 +82,8 @@ public class SQLAppDatabase : IAppDatabase
             }
             catch (SQLiteException e)
             {
+                Debug.WriteLine("{msg}", e.StackTrace);
+                Debug.WriteLine("{msg}", e.Message);
                 throw;
             }
 
@@ -85,44 +94,64 @@ public class SQLAppDatabase : IAppDatabase
         r.Landmarks = builder.ToString();
         try
         {
-            _ = await _database.InsertAsync(builder);
+            _ = await _database.InsertAsync(r);
         }
         catch (SQLiteException e)
         {
+            Debug.WriteLine("{msg}", e.StackTrace);
+            Debug.WriteLine("{msg}", e.Message);
             throw;
         }
     }
 
     public async Task AddDescriptionAsync(Sight sight, string locale, string description)
     {
-        await InsertDescriptionAsync("DescriptionL_" + locale, sight.SightPin.Address, description);
+        await InsertDescriptionAsync("DescriptionL_" + locale, true, sight.SightPin.Address, description);
     }
 
     public async Task AddDescriptionAsync(Tour tour, string locale, string description)
     {
-        await InsertDescriptionAsync("DescriptionR_" + locale, tour.Name, description);
+        await InsertDescriptionAsync("DescriptionR_" + locale, false, tour.Name, description);
     }
 
-    private async Task InsertDescriptionAsync(string tableName, string itemName, string description)
+    private async Task InsertDescriptionAsync(string tableName, bool isLandmark, string itemName, string description)
     {
         try
         {
             _ = await _database.QueryAsync<int>(
-                @"CREATE TABLE IF NOT EXISTS ? (
+                @"CREATE TABLE IF NOT EXISTS " + tableName + @" (
 ID INTEGER PRIMARY KEY,
 Text TEXT
-);", tableName);
+);");
 
-            List<Landmark> res = await _database.QueryAsync<Landmark>(@"SELECT * FROM ? WHERE Name = ?",
-                tableName, itemName);
-            // TODO improve exception
-            if (res.Count != 1) throw new Exception("Invalid landmark");
+            int Id = 0;
+            if (isLandmark)
+            {
+                List<Landmark> res = await _database.QueryAsync<Landmark>(
+                    @"SELECT * FROM Landmarks WHERE Name = ?",
+                    itemName);
+                // TODO improve exception
+                if (res.Count != 1) throw new Exception("Invalid landmark");
+                Id = res[0].ID;
+            }
+            else
+            {
+                List<Route> res = await _database.QueryAsync<Route>(
+                    @"SELECT * FROM Routes WHERE Name = ?",
+                    itemName);
+                // TODO improve exception
+                if (res.Count != 1) throw new Exception("Invalid Route");
+                Id = res[0].ID;
+            }
 
-            _ = await _database.QueryAsync<int>(@"INSERT INTO ? (ID, Text) VALUES (?, ?)",
-                tableName, res[0].ID, description);
+
+            _ = await _database.QueryAsync<int>(@"INSERT INTO " + tableName + @" (ID, Text) VALUES (?, ?)",
+                Id, description);
         }
         catch (SQLiteException e)
         {
+            Debug.WriteLine("{msg}", e.StackTrace);
+            Debug.WriteLine("{msg}", e.Message);
             throw;
         }
     }
@@ -147,22 +176,26 @@ Text TEXT
             List<Route> r = await _database.QueryAsync<Route>(@"SELECT * FROM Routes WHERE name = ?", name);
             if (r.Count != 1) throw new Exception("Invalid route");
             List<Description> desc =
-                await _database.QueryAsync<Description>(@"SELECT * FROM ? WHERE ID = ?", tableNameRoute, r[0].ID);
+                await _database.QueryAsync<Description>(@"SELECT * FROM " + tableNameRoute + @" WHERE ID = ?", r[0].ID);
             if (desc.Count == 1) description = desc[0].Text;
             string[] lms = r[0].Landmarks.Split(";");
             foreach (string id in lms)
             {
+                if (string.IsNullOrEmpty(id)) continue;
                 List<Landmark> lm = await _database.QueryAsync<Landmark>(@"SELECT * FROM Landmarks WHERE ID = ?", id);
                 List<Description> lmDesc =
-                    await _database.QueryAsync<Description>(@"SELECT * FROM ? WHERE ID = ?", tableNameLandmark, id);
-                if (lm.Count != 1) throw new Exception("Invalid route");
+                    await _database.QueryAsync<Description>(@"SELECT * FROM " + tableNameLandmark + @" WHERE ID = ?",
+                        id);
+                if (lm.Count != 1) throw new Exception("Invalid Landmark (route)");
                 Sight sight = new Sight(lm[0].Name, new Location(lm[0].Latitude, lm[0].Longitude),
                     lmDesc.Count != 1 ? "" : lmDesc[0].Text);
                 sights.Add(sight);
             }
         }
-        catch
+        catch (Exception e)
         {
+            Debug.WriteLine("{msg}", e.StackTrace);
+            Debug.WriteLine("{msg}", e.Message);
             throw;
         }
 
@@ -178,7 +211,7 @@ Text TEXT
     [Table("Landmarks")]
     private class Landmark
     {
-        [PrimaryKey] public int ID { get; set; }
+        [PrimaryKey, AutoIncrement] public int ID { get; set; }
         public string Name { get; set; }
         public string ImagePath { get; set; }
         public double Latitude { get; set; }
@@ -188,7 +221,7 @@ Text TEXT
     [Table("Routes")]
     private class Route
     {
-        [PrimaryKey] public int ID { get; set; }
+        [PrimaryKey, AutoIncrement] public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Landmarks { get; set; }
