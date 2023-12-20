b9fa68af674f2e3ebc5ec241ef28fd05981b848d
Sam-D-C <114139132+Sam-D-C@users.noreply.github.com>
2023-12-20 12:26:07 +0100

    Xunit-tests
    Classes: Models.Sight & Models.Tour

--- VVVoyage/TestProject/Tests/ModelTests.cs
+++ VVVoyage/TestProject/Tests/ModelTests.cs
@@ -4,8 +4,9 @@ namespace TestProject.Tests
 {
     public class ModelsTest
     {
+        //Sight.CS
         [Fact]
-        public void Label_of_SightPin_should_be_set_when_not_entered()
+        private void Label_of_SightPin_should_be_set_when_not_entered()
         {
             Location l = new Location();
             var sut = new Sight("adress", l, "sightDescription");
@@ -14,7 +15,7 @@ namespace TestProject.Tests
         }
 
         [Fact]
-        public void Label_of_SightPin_should_be_entered_string()
+        private void Label_of_SightPin_should_be_entered_string()
         {
             Location l = new Location();
             var sut = new Sight("LabelString", "adress", l, "sightDescription");
@@ -23,7 +24,7 @@ namespace TestProject.Tests
         }
 
         [Fact]
-        public void Address_of_SightPin_should_be_entered_string_StandardLabel()
+        private void Address_of_SightPin_should_be_entered_string_StandardLabel()
         {
             Location l = new Location();
             var sut = new Sight("address", l, "sightDescription");
@@ -32,7 +33,7 @@ namespace TestProject.Tests
         }
 
         [Fact]
-        public void Address_of_SightPin_should_be_entered_string_PersonalLabel()
+        private void Address_of_SightPin_should_be_entered_string_PersonalLabel()
         {
             Location l = new Location();
             var sut = new Sight("LabelString", "address", l, "sightDescription");
@@ -41,7 +42,7 @@ namespace TestProject.Tests
         }
 
         [Fact]
-        public void Location_of_SightPin_should_be_entered_Location_StandardLabel()
+        private void Location_of_SightPin_should_be_entered_Location_StandardLabel()
         {
             Location l = new Location();
             var sut = new Sight("address", l, "sightDescription");
@@ -50,7 +51,7 @@ namespace TestProject.Tests
         }
 
         [Fact]
-        public void Location_of_SightPin_should_be_entered_Location_PersonalLabel()
+        private void Location_of_SightPin_should_be_entered_Location_PersonalLabel()
         {
             Location l = new Location();
             var sut = new Sight("LabelString", "address", l, "sightDescription");
@@ -59,7 +60,7 @@ namespace TestProject.Tests
         }
 
         [Fact]
-        public void SightDesciption_of_SightPin_should_be_entered_SightDescpription_StandardLabel()
+        private void SightDesciption_of_SightPin_should_be_entered_SightDescpription_StandardLabel()
         {
             Location l = new Location();
             var sut = new Sight("address", l, "sightDescription");
@@ -68,12 +69,95 @@ namespace TestProject.Tests
         }
 
         [Fact]
-        public void SightDesciption_of_SightPin_should_be_entered_SightDescpription_PersonalLabel()
+        private void SightDesciption_of_SightPin_should_be_entered_SightDescpription_PersonalLabel()
         {
             Location l = new Location();
             var sut = new Sight("LabelString", "address", l, "sightDescription");
 
             Assert.Equal("sightDescription", sut.SightDescription);
+        }
+
+        [Fact]
+        private void Sight_Label_Location_SightDescription_StandardLabel()
+        {
+            Location l = new Location();
+            var sut = new Sight("address", l, "sightDescription");
+
+            Assert.Equal("address", sut.SightPin.Address);
+            Assert.Equal(l, sut.SightPin.Location);
+            Assert.Equal("sightDescription", sut.SightDescription);
+        }
+
+        [Fact]
+        private void Sight_Label_Location_SightDescription_PersonalLabel()
+        {
+            Location l = new Location();
+            var sut = new Sight("labelString","address", l, "sightDescription");
+
+            Assert.Equal("labelString", sut.SightPin.Label);
+            Assert.Equal("address", sut.SightPin.Address);
+            Assert.Equal(l, sut.SightPin.Location);
+            Assert.Equal("sightDescription", sut.SightDescription);
+        }
+
+
+        //Tour.CS
+        [Fact]
+        private void Tour_name_should_be_entered_string()
+        {
+            Location l = new Location();
+
+            var one = new Sight("sightName", l, "sightDescription");
+
+            var sut = new Tour("name", "description", one);
+
+            Assert.Equal("name", sut.Name);
+        }
+
+        [Fact]
+        private void Tour_descpription_should_be_entered_string()
+        {
+            Location l = new Location();
+
+            var one = new Sight("sightName", l, "sightDescription");
+
+            var sut = new Tour("name", "description", one);
+
+            Assert.Equal("description", sut.Description);
+        }
+
+        [Fact]
+        private void Tour_landmarks_should_be_entered_landmarks()
+        {
+            Location l = new Location();
+
+            var one = new Sight("sightName", l, "sightDescription");
+            var two = new Sight("nameSight", l, "descriptionSight");
+            var three = new Sight("name", l, "description");
+
+            Sight[] sightArray = { one, two, three };
+
+            var sut = new Tour("tourName", "tourDescription", one, two, three);
+
+            Assert.Equal(sightArray, sut.Landmarks);
+        }
+
+        [Fact]
+        private void Tour_Name_Description_Landmarks()
+        {
+            Location l = new Location();
+
+            var one = new Sight("sightName", l, "sightDescription");
+            var two = new Sight("nameSight", l, "descriptionSight");
+            var three = new Sight("name", l, "description");
+
+            Sight[] sightArray = { one, two, three };
+
+            var sut = new Tour("tourName", "tourDescription", one, two, three);
+
+            Assert.Equal(sightArray, sut.Landmarks);
+            Assert.Equal("tourName", sut.Name);
+            Assert.Equal("tourDescription", sut.Description);
         }
     }
 }
